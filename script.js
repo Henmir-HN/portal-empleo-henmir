7c29bde0595b620a98bce0bf5078c052b6d2e84c
Henmir-HN <agencia.henmir@gmail.com>
2025-08-18 21:46:18 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Global Configuration
 //const API_BASE_URL = 'http://34.68.183.117.nip.io/public-api';
-const API_BASE_URL = 'https://34.68.183.117.sslip.io/public-api'
+const API_BASE_URL = 'https://34.68.183.117.sslip.io/public-api/vacancies'
 let appData = {
     vacancies: [],
     posts: [],
